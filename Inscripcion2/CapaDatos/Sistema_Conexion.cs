747f27f815c166f966f421d6ac36e9054801eb90
ManuelGTM <manuelgabrielt@gmail.com>
2023-07-24 18:26:21 -0400

    Creacion de un nuevo proyecto hasta la capa negocio

---
+++ Inscripcion2/CapaDatos/Sistema_Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace CapaDatos
+{
+    public class Sistema_Conexion
+    {
+        public static string miconexion = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\MT\workbench\Miproyecto\Inscripcion_MT\Inscripcion2\CapaDatos\CapaDatos.mdf;Integrated Security=True";
+
+        public SqlConnection dbconexion = new SqlConnection(miconexion);
+    }
+}
---
+++ Inscripcion2/CapaNegocio/CNEstudiante.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.Sql;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class CNEstudiante
+    {
+        public static string InsertarEstudiante(string Nombre, string Apellido, int IdTutor,string Sexo, string FechaNacimiento, string Direccion, string Estado)
+        {
+            CDEstudiante objEstudiante = new CDEstudiante();
+
+            objEstudiante._Nombre = Nombre;
+            objEstudiante._Apellido = Apellido;
+            objEstudiante._IdTutor = IdTutor;
+            objEstudiante._Sexo = Sexo;
+            objEstudiante._FechaNacimiento = FechaNacimiento;
+            objEstudiante._Direccion = Direccion;
+            objEstudiante._Estado = Estado;
+
+            return objEstudiante.InsertarEstudiante(objEstudiante);
+        }
+
+        public static string ActualizarEstudiante(int IdEstudiante, string Nombre, string Apellido, int IdTutor,string Sexo, string FechaNacimiento, string Direccion, string Estado)
+        {
+            CDEstudiante objEstudiante = new CDEstudiante();
+
+            objEstudiante._IdEstudiante = IdEstudiante;
+            objEstudiante._Nombre = Nombre;
+            objEstudiante._Apellido = Apellido;
+            objEstudiante._IdTutor = IdTutor;
+            objEstudiante._Sexo = Sexo;
+            objEstudiante._FechaNacimiento = FechaNacimiento;
+            objEstudiante._Direccion = Direccion;
+            objEstudiante._Estado = Estado;
+
+            return objEstudiante.ActualizarEstudiante(objEstudiante);
+        }
+
+        public DataTable ObtenerEstudiante(string miparametro)
+        {
+            CDEstudiante objEstudiante = new CDEstudiante();
+            DataTable dt = new DataTable();
+
+            dt = objEstudiante.DataTableEstudiante(miparametro);
+
+            return dt;
+        }
+    }
+}
